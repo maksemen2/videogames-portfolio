565614c77e93ece21ddd680d1fc9ef54cf6df62f
maksemen2 <lxshnaps@gmail.com>
2025-06-10 00:08:57 +0300

    completed js scripts and assets

---
+++ js/swiper.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.artifact-swiper', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    initialSlide: 1,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    keyboard: {
+        enabled: true,
+    },
+    mousewheel: {
+        thresholdDelta: 50,
+    },
+});
